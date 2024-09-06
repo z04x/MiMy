f8a0167621a502a6a6520748c323f136d63bf9ea
z04x <ninpetrik@gmail.com>
2024-09-06 16:57:11 +0200

    fix: homePage btn Text

--- src/components/HomeScreen.tsx
+++ src/components/HomeScreen.tsx
@@ -117,7 +117,7 @@ const HomeScreen: React.FC = () => {
               <img src="https://chat-agregator.s3.eu-central-1.amazonaws.com/svg-logos/premium-icon.svg" alt="Premium" />
             </Typography>
             <Typography sx={{fontSize: '17px', fontWeight: '400', lineHeight: '22px', textAlign: 'left', color: '#FFFFFFA3',}}>
-              {isPremium ? "Продовать через текст никогда не было так просто" : "Требуется премиум-подписка"}
+              Продовать через текст никогда не было так просто
             </Typography>
           </Box>
         </Button>
